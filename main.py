b4c2955235b3fb531c5aca5ffc05b56b9bbf19d7
Arjun Krishna Babu <arjunkrishnababu96@gmail.com>
2020-08-05 22:46:51 +0530

    make the function return a boolean to denote whether the site is up

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
 import requests
 
-def check_site(url):
+def is_site_working(url):
     try:
-        r = requests.head(url, timeout=5)
-        status = 'ONLINE' if r.status_code == 200 else 'OFFLINE'
-    except:
-        status = 'OFFLINE'
-    print("{} {}".format(status, url))
+        r = requests.head(url, timeout=2)
+        r.raise_for_status()
+        return True if r.status_code == 200 else False
+    except requests.exceptions.RequestException as e:
+        pass
+
+    return False
 
 def main():
     with open('sitelist.txt') as f:
         for site in f:
-            check_site(site.strip())
+            print(is_site_working(site.strip()))
 
 if __name__ == "__main__":
     main()
