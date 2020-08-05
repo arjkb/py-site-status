eaa5f9a900562821933e3529cddbfd705a1549e3
Arjun Krishna Babu <arjunkrishnababu96@gmail.com>
2020-08-05 22:30:34 +0530

    define function to check and print if a given site is running

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
+import requests
+
+def check_site(url):
+    try:
+        r = requests.head(url, timeout=5)
+        status = 'ONLINE' if r.status_code == 200 else 'OFFLINE'
+    except:
+        status = 'OFFLINE'
+    print("{} {}".format(status, url))
+
 def main():
     with open('sitelist.txt') as f:
         for site in f:
-            print(site)
+            check_site(site.strip())
 
 if __name__ == "__main__":
     main()
